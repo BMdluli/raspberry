51478264094b1f011e19d86947661ec939e0fd87
Bekithemba Mdluli <putthetrust.gamedev@gmail.com>
2024-09-13 12:00:50 +0200

    removed firebas auth

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,17 @@
 require("dotenv").config();
 const express = require("express");
-const cookieParser = require("cookie-parser");
-const router = require("./routes");
+const authRouter = require("./routes/authRoutes");
+
+// Clerk({ apiKey: process.env.CLERK_SECRET_KEY });
 
 const app = express();
 const PORT = process.env.PORT | 3000;
 
 // MIDDLEWARE
 app.use(express.json());
-app.use(cookieParser());
 
 // ROUTES
-app.use(router);
+app.use("/api", authRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
